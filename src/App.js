c939664e8bc979e2edbdf3b785b651e37a4de786
R-Tomishi <43924379+R-Tomishi@users.noreply.github.com>
2022-04-03 17:15:57 +0900

    0

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import "./styles.css";
+import axios from "axios";
+
+const onClickUsers = () => {
+  axios
+    .get("https://jsonplaceholder.typicode.com/users")
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((err) => console.log(err));
+};
+
+const onClickUser1 = () => {
+  axios
+    .get("https://jsonplaceholder.typicode.com/users/1")
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((err) => console.log(err));
+};
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={onClickUsers}>Users</button>
+      <button onClick={onClickUser1}>User1</button>
     </div>
   );
 }
